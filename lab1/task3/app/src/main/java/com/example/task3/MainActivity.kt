36a744efab39852a24abbc2b5a8c63b551b0ed1d
spAm25 <39336091+spAm25@users.noreply.github.com>
2021-02-15 01:00:04 +0300

    задача 3 готова

---
+++ lab1/task3/app/src/main/java/com/example/task3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.task3
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity_layout)
+    }
+}
